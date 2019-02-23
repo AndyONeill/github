ee512984337266411858580ada086e255faece38
Andrew <Andrew@DESKTOP-NEE2813>
2019-02-23 18:21:29 +0000

    Fruits new project

---
+++ wpf_EnumBinding/EnumBinding.TheEnum/Fruits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumBinding.TheEnum
+{
+    public enum Fruits
+    {
+        Apple,
+        Banana,
+        Grapes,
+        Melon,
+        Pear
+    }
+}
