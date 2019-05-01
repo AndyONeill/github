389ced5f6e48602e32e8ebd23c9416dc53d9fb80
Andrew <Andrew@DESKTOP-NEE2813>
2019-05-01 11:14:48 +0100

    GitHubRepos Break out javascript functions

--- GitHubTable/app.js
+++ GitHubTable/app.js
@@ -4,60 +4,65 @@ $(document).ready(function() {
     $('#getNewURL').on('submit', function(e) { 
         e.preventDefault();  //prevent form from submitting
         gitHubUrl = $('#urlInput').val();
-        AjaxCall();
+        ajaxRequest(ajaxOK, ajaxFail, gitHubUrl);
     });
 });
 
 $(document).ready(function(){
-	AjaxCall();
+    jQuery.support.cors = true;
+    ajaxRequest(ajaxOK, ajaxFail, gitHubUrl);
 });
 
-function AjaxCall(){
-    jQuery.support.cors = true;
+function ajaxRequest (s,f, url){
     $.ajax({
         type: "GET",
-        url: gitHubUrl,
+        url: url,
         contentType: "application/json; charset=utf-8", 
         dataType: 'json',
-        success: function (response) {
-            $("#repoTable tbody").empty();
-            $.each(response, function(i,r){ 
-                function WrapWithCell(content){
-                    return "<td>" + content + "</td>"
-                };   
-                
-                function WrapWithAnchor(url){
-                    return "<a href='" + url + "'> <div class='fullSize'>" + url + "</div></a>"
-                };
+        success: s,
+        error: f
+    });
+};
 
-                $('#repoTable').append(
-                        "<tr>"
-                        + WrapWithCell(r.login)
-                        + WrapWithCell(r.id)
-                        + WrapWithCell(WrapWithAnchor(r.repos_url))
-                        + WrapWithCell(WrapWithAnchor(r.issues_url))
-                        +"</tr>" )
-                    })
-            },
-        error: function (jqXHR, exception) {
-            $("#repoTable tbody").empty();
-            console.log(jqXHR);
-            var msg = '';
-            if (jqXHR.status === 0) {
-                msg = 'Not connected.\n Please check Network connection or try later.';
-            } else if (jqXHR.status == 404) {
-                msg = 'Requested page not found. [404]';
-            } else if (jqXHR.status == 500) {
-                msg = 'Internal Server Error [500].';
-            } else if (exception === 'parsererror') {
-                msg = 'Requested JSON parse failed.';
-            } else if (exception === 'timeout') {
-                msg = 'Time out error.';
-            } else if (exception === 'abort') {
-                msg = 'Ajax request aborted.';
-            } else {
-                msg = 'Uncaught Error.\n' + jqXHR.responseText;
-            }
-            alert(msg);
+function ajaxOK (response) {
+    function WrapWithCell(content){
+        return "<td>" + content + "</td>"
+    };   
+    function WrapWithAnchor(url){
+        return "<a href='" + url + "'> <div class='fullSize'>" + url + "</div></a>"
+    };
+
+    $("#repoTable tbody").empty();
+    $.each(response, function(i,r){ 
+        $('#repoTable').append(
+                "<tr>"
+                + WrapWithCell(r.login)
+                + WrapWithCell(r.id)
+                + WrapWithCell(WrapWithAnchor(r.repos_url))
+                + WrapWithCell(WrapWithAnchor(r.issues_url))
+                +"</tr>" )
+            })
+    };
+             
+             
+function ajaxFail  (jqXHR, exception) {
+    $("#repoTable tbody").empty();
+    console.log(jqXHR);
+    var msg = '';
+    if (jqXHR.status === 0) {
+        msg = 'Not connected.\n Please check Network connection or try later.';
+    } else if (jqXHR.status == 404) {
+        msg = 'Requested page not found. [404]';
+    } else if (jqXHR.status == 500) {
+        msg = 'Internal Server Error [500].';
+    } else if (exception === 'parsererror') {
+        msg = 'Requested JSON parse failed.';
+    } else if (exception === 'timeout') {
+        msg = 'Time out error.';
+    } else if (exception === 'abort') {
+        msg = 'Ajax request aborted.';
+    } else {
+        msg = 'Uncaught Error.\n' + jqXHR.responseText;
     }
-})};
+    alert(msg);
+};
